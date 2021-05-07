a3793d9687463a51a0a0018c58837c10acff0fb1
Adnan <adnan.museel123@gmail.com>
2021-05-07 19:44:53 +0500

    footer todos todo components deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import Header from "./mycomponents/Header";
+import { Todos } from "./mycomponents/Todos";
+import { Footer } from "./mycomponents/Footer";
+import { TodoItem } from "./mycomponents/TodoItem";
+import React, {useState} from 'react'; 
 
 function App() {
+  let onDelete = (todo)=>{
+    console.log("i am on delete of todo ",todo)
+    setTodos(todos.filter((e)=>{
+        return e!==todo;
+    }))
+  }
+
+  const [todos,setTodos]=useState([
+    {
+      sno:1,
+      title:'Go to the market',
+      desc: 'You need to go to the market to get this job done'
+    },
+    {
+      sno:2,
+      title:'Go to the gym',
+      desc: 'You need to go to the market to get this job done'
+    },
+    {
+      sno:3,
+      title:'Go to the club',
+      desc: 'You need to go to the market to get this job done'
+    }
+  ]);
+
   return (
     <>
-    <Header/>
-   </>
+      <Header title="MyTodoList" searchBar={false}/>
+      <Todos todos = {todos} onDelete = {onDelete} />
+      <Footer />
+    </>
   );
 }
 
--- src/mycomponents/Header.js
+++ src/mycomponents/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
-export default function Header() {
+export default function Header(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Todo App</a>
+        <a className="navbar-brand" href="#">{props.title}</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -18,12 +19,24 @@ export default function Header() {
             </li>
            
           </ul>
-          <form className="form-inline my-2 my-lg-0">
+          {props.searchBar?<form className="form-inline my-2 my-lg-0">
             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
             <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
+          </form>:""}
         </div>
       </nav>
         
     )
 }
+
+Header.defaultProp = {
+  title: "Your title here",
+  searchBar: true
+}
+
+
+Header.propTypes = {
+  title:PropTypes.string,
+  searchBar: PropTypes.bool.isRequired
+}
+
